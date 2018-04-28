1b6876e6c1ea535971b878cf2e4501f86550264c
Simon Žlender <simon.zlender@gmail.com>
2018-04-28 12:30:33 +0200

    Add reveal/continue_reveal/flag actions to Mina.Grid.Worker

--- lib/mina/grid/worker.ex
+++ lib/mina/grid/worker.ex
@@ -25,15 +25,74 @@ defmodule Mina.Grid.Worker do
     with :undefined <- Swarm.whereis_name(name),
          {:ok, pid} <- Swarm.register_name(name, supervisor, :start_child, [args]) do
       Swarm.join(:grid, pid)
-      {:ok, pid}
+      pid
     else
-      pid when is_pid(pid) -> {:ok, pid}
-      {:error, {:already_registered, pid}} -> {:ok, pid}
-      {:error, reason} -> {:error, reason}
+      pid when is_pid(pid) -> pid
+      {:error, {:already_registered, pid}} -> pid
     end
   end
 
   def list() do
     Swarm.members(:grid)
+  end
+
+  def handle_cast({:flag, pos, player_id}, state) do
+    IO.inspect({:flag, pos, player_id})
+
+    state =
+      case flag_pos(state, pos, player_id) do
+        {:ok, moves} -> %{state | moves: Map.merge(state.moves, moves)}
+        {:error, _reason} -> state
+      end
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:reveal, pos, player_id}, state) do
+    IO.inspect({:reveal, pos, player_id})
+
+    state =
+      case reveal_pos(state, pos, player_id) do
+        {:ok, state} -> state
+        {:error, _reason} -> state
+      end
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:continue_reveal, deferred, player_id}, state) do
+    IO.inspect({:continue_reveal, deferred, player_id})
+
+    state =
+      Enum.reduce(deferred, state, fn pos, state ->
+        case reveal_pos(state, pos, player_id) do
+          {:ok, state} -> state
+          {:error, _reason} -> state
+        end
+      end)
+
+    {:noreply, state}
+  end
+
+  defp flag_pos(state, pos, player_id) do
+    with {:ok, {moves, []}} <- Mina.Grid.flag(state.grid, pos, player_id) do
+      state = update_in(state.grid.moves, &Map.merge(&1, moves))
+      {:ok, state}
+    end
+  end
+
+  defp reveal_pos(state, pos, player_id) do
+    with {:ok, {moves, deferred}} <- Mina.Grid.reveal(state.grid, pos, player_id) do
+      offset_fun = fn {_, offset} -> offset end
+      pos_fun = fn {pos, _} -> pos end
+      deferred_offset = Mina.Grid.offset_deferred(state.grid, deferred)
+
+      for {offset, pos_list} <- Enum.group_by(deferred_offset, offset_fun, pos_fun) do
+        GenServer.cast(fetch(offset), {:continue_reveal, pos_list, player_id})
+      end
+
+      state = update_in(state.grid.moves, &Map.merge(&1, moves))
+      {:ok, state}
+    end
   end
 end
